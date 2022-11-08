4bf5d8c1c35d8cfd24b47a6f7869757bd373b7f7
ArtyomMasen <osenkoffartyom@gmail.com>
2022-11-08 20:51:11 +0300

    added API property for password in swagger

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -3,13 +3,15 @@ import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
-  @ApiProperty({example: 1, description: 'this is ID maaaan'})
+  @ApiProperty({example: 1, description: 'Unique ID'})
   @PrimaryGeneratedColumn()
   id: string;
 
-  @ApiProperty({example: 'Example123', description: 'this is ID maaaan'})
+  @ApiProperty({example: 'Example123', description: 'Write your login'})
   @Column()
   login: string;
+
+  @ApiProperty({example: '123321qwe', description: 'Write your password'})
   @Column()
   password: string;
   @Column()
